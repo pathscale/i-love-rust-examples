4bc6ed694e7f5cf96a5cef5217c70ca123aaf957
qiujiangkun <qiujiangkun@foxmail.com>
2021-08-29 19:34:44 +0800

    Make types explicit

--- src/analyze/mod.rs
+++ src/analyze/mod.rs
@@ -20,22 +20,13 @@ pub fn thread2(mut rx_t1: mpsc::Receiver<String>, tx_t2: mpsc::Sender<YourDataSt
          }
 
          */
-        let mut id = vec![];
-        let mut name = vec![];
-        let mut username = vec![];
-        let mut email = vec![];
-        let mut street = vec![];
-        let mut suite = vec![];
-        let mut city = vec![];
-        let mut zipcode = vec![];
-        let mut lat = vec![];
-        let mut lng = vec![];
-        let mut phone = vec![];
-        let mut website = vec![];
-        let mut company_name = vec![];
-        let mut company_catch_phrase = vec![];
-        let mut company_bs = vec![];
-        let mut insertion_time = vec![];
+        let mut id: Vec<i64> = vec![];
+        let mut name: Vec<String> = vec![];
+        let mut username: Vec<String> = vec![];
+        let mut email: Vec<String> = vec![];
+        let mut phone: Vec<String> = vec![];
+        let mut website: Vec<String> = vec![];
+        let mut insertion_time: Vec<String> = vec![];
         fn extract_string(ser: &mut Vec<String>, item: &ijson::IValue, key: &str) -> anyhow::Result<()> {
             ser.push(item.get(key).with_context(|| format!("Missing {}", key))?.as_string().with_context(|| format!("{} is not string", key))?.to_string());
             Ok(())
@@ -46,21 +37,11 @@ pub fn thread2(mut rx_t1: mpsc::Receiver<String>, tx_t2: mpsc::Sender<YourDataSt
             extract_string(&mut name, item, "name")?;
             extract_string(&mut username, item, "username")?;
             extract_string(&mut email, item, "email")?;
-            let address = item.get("address").context("Missing address")?;
-            extract_string(&mut street, address, "street")?;
-            extract_string(&mut suite, address, "suite")?;
-            extract_string(&mut city, address, "city")?;
-            extract_string(&mut zipcode, address, "zipcode")?;
             extract_string(&mut website, item, "website")?;
             extract_string(&mut phone, item, "phone")?;
-            let geo = address.get("geo").context("Missing geo")?;
-            extract_string(&mut lat, geo, "lat")?;
-            extract_string(&mut lng, geo, "lng")?;
+
+            // current insertion time
             insertion_time.push(chrono::Utc::now().to_rfc3339());
-            let company = item.get("company").context("Missing company")?;
-            extract_string(&mut company_name, company, "name")?;
-            extract_string(&mut company_catch_phrase, company, "catchPhrase")?;
-            extract_string(&mut company_bs, company, "bs")?;
         }
         let frame = DataFrame::new(
             vec![
@@ -68,17 +49,8 @@ pub fn thread2(mut rx_t1: mpsc::Receiver<String>, tx_t2: mpsc::Sender<YourDataSt
                 Series::new(name),
                 Series::new(username),
                 Series::new(email),
-                Series::new(street),
-                Series::new(suite),
-                Series::new(city),
-                Series::new(zipcode),
-                Series::new(lat),
-                Series::new(lng),
                 Series::new(phone),
                 Series::new(website),
-                Series::new(company_name),
-                Series::new(company_catch_phrase),
-                Series::new(company_bs),
                 Series::new(insertion_time)]);
         frame.print();
         //do your first data processing here...
