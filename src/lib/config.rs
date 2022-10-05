017c267a7c3c745d8f8dceec8738f7fa30b3f902
sergera <sergera88@gmail.com>
2022-10-05 01:07:53 -0300

    config file as default environment values
    - added default values for all services to config.json
    
    - changed lib/config.rs to use config.json as default and overwrite them in
    case they are passed as command line arguments

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -1,10 +1,10 @@
-use crate::database::DatabaseConfig;
 use crate::log::LogLevel;
 use clap::Parser;
 use eyre::*;
 use serde::*;
 use std::env::current_dir;
 use std::path::PathBuf;
+
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct CliArgument {
@@ -18,25 +18,32 @@ struct CliArgument {
         env = "CONFIG"
     )]
     config: PathBuf,
-    #[clap(long, default_value = "INFO", env = "LOG_LEVEL")]
-    log_level: LogLevel,
-    #[clap(long, default_value = "localhost", env = "HOST")]
+    #[clap(long, env = "LOG_LEVEL")]
+    log_level: Option<LogLevel>,
+    #[clap(long, env = "HOST")]
     /// The host to listen on
-    host: String,
-    #[clap(long, default_value = "8888", env = "PORT")]
+    host: Option<String>,
+    #[clap(long, env = "PORT")]
     /// The port to listen on
-    port: u16,
-    #[clap(long, default_value = "", env = "PUB_CERT")]
-    pub_cert: String,
-    #[clap(long, default_value = "", env = "PRIV_CERT")]
-    priv_cert: String,
+    port: Option<u16>,
+    #[clap(long, env = "PUB_CERT")]
+    pub_cert: Option<String>,
+    #[clap(long, env = "PRIV_CERT")]
+    priv_cert: Option<String>,
 }
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Config {
-    pub db: DatabaseConfig,
-    #[serde(skip)]
+    pub db: DbConfig,
     pub app: AppConfig,
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DbConfig {
+    pub host: String,
+    pub port: u32,
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct AppConfig {
     pub name: String,
@@ -46,19 +53,62 @@ pub struct AppConfig {
     pub pub_cert: String,
     pub priv_cert: String,
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+pub struct AppFileConfig {
+    pub log_level: LogLevel,
+    pub host: String,
+    pub port: u16,
+    pub pub_cert: String,
+    pub priv_cert: String,
+}
+
 pub fn load_config(service_name: String) -> Result<Config> {
     let args: CliArgument = CliArgument::parse();
 
     println!("Working directory {}", current_dir()?.display());
     println!("Loading config from {}", args.config.display());
+
     let config = std::fs::read_to_string(&args.config)?;
-    let mut config: Config = serde_json::from_str(&config)?;
-    config.app.log_level = args.log_level;
-    config.app.port = args.port;
-    config.app.host = args.host;
-    config.app.name = service_name;
-    config.app.pub_cert = args.pub_cert;
-    config.app.priv_cert = args.priv_cert;
-    println!("App config {:#?}", config.app);
-    Ok(config)
+    let root: serde_json::Value = serde_json::from_str(&config)?;
+
+    let db: DbConfig = serde_json::from_value(root.get("db").unwrap().to_owned())?;
+    let app_file: AppFileConfig = serde_json::from_value(
+        root.get("app")
+            .unwrap()
+            .get(&service_name)
+            .unwrap()
+            .to_owned(),
+    )?;
+
+    let mut app: AppConfig = AppConfig::default();
+    app.log_level = if args.log_level.is_some() {
+        args.log_level.unwrap()
+    } else {
+        app_file.log_level
+    };
+    app.port = if args.port.is_some() {
+        args.port.unwrap()
+    } else {
+        app_file.port
+    };
+    app.host = if args.host.is_some() {
+        args.host.unwrap()
+    } else {
+        app_file.host
+    };
+    app.pub_cert = if args.pub_cert.is_some() {
+        args.pub_cert.unwrap()
+    } else {
+        app_file.pub_cert
+    };
+    app.priv_cert = if args.priv_cert.is_some() {
+        args.priv_cert.unwrap()
+    } else {
+        app_file.priv_cert
+    };
+    app.name = service_name;
+    println!("App config {:#?}", app);
+
+    Ok(Config { db: db, app: app })
 }
